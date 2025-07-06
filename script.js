9892d38f7ff1e532b408b8e10db9cb4713dadd05
Pratyush-0803 <pratyushdixit385@gmail.com>
2025-07-06 23:12:45 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", ()=> {
-    
-})
+    function scrollbar(parent,child){
+        let hidetime;
+        parent.addEventListener("mouseenter",()=> {
+            clearTimeout(hidetime);
+            child.style.backgroundColor = "rgb(101, 101, 101)";
+            child.style.pointerEvents = "auto";
+        });
+        parent.addEventListener("mouseleave", ()=> {
+            hidetime = setTimeout(()=> {
+                child.style.backgroundColor = "transparent";
+                child.style.pointerEvents = "none";
+            },850);
+        })
+    }
+    scrollbar(document.querySelector(".playlistwrapper"),document.querySelector(".scrollbar1"));
+    scrollbar(document.querySelector(".rightpartwrapper"),document.querySelector(".scrollbar2"));
+});
